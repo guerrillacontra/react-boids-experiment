3d324bc2ad80aeaf4566eb2cc2fe3ed134c7350b
James Wrightson <james.p.wrightson@gmail.com>
2022-08-10 15:26:46 +0100

    Project Setup
    Vite React TS build with TailwindCSS 3 linked up

---
+++ boids/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from "react";
+
+function App() {
+
+  return (
+    <div className="App">
+        <div className="flex justify-center mt-4">
+            <h1 className="font-bold text-2xl text-blue-900 border rounded-xl p-4">
+                Boids!
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
